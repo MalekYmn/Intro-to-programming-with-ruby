c7ebe36b139a9c4398b6e3f892cb53058c815bbc
Malek Ymn <malekymn@hotmail.com>
2020-09-22 16:47:55 +0000

    Exercises in 'The Basics' chapter

---
+++ the_basics/exercise3.rb
@@ -0,0 +1,12 @@
+movies = { "One Flew Over the Cuckoo's Nest": 1975,
+           "Howl's Moving Castle": 2004,
+           "The Wolf of Wall Street": 2013,
+           "Spirited Away": 2001,
+           "Das Boot": 1981 }
+           
+puts movies[:"One Flew Over the Cuckoo's Nest"]
+puts movies[:"Howl's Moving Castle"]
+puts movies[:"The Wolf of Wall Street"]
+puts movies[:"Spirited Away"]
+puts movies[:"Das Boot"]
+
